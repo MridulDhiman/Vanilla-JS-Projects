e7b9044951b41eb259247008e6bb0d1247142c2b
Mridul Dhiman <60432617+MridulDhiman@users.noreply.github.com>
2023-07-09 02:56:58 +0530

    drum kit upload

---
+++ drum-kit/README.md
@@ -0,0 +1,4 @@
+# drum kit
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mriduldhiman/pen/wvPaXaP](https://codepen.io/mriduldhiman/pen/wvPaXaP).
+
---
+++ drum-kit/script.js
@@ -0,0 +1,55 @@
+const item1 = document.getElementById("item1");
+
+item1.addEventListener("click", function () {
+  const audioClick1 = new Audio("./audio/cat.mp3");
+  audioClick1.play();
+});
+
+const item2 = document.getElementById("item2");
+
+item2.addEventListener("click", function () {
+  const audioClick2 = new Audio("./audio/cow.mp3");
+  audioClick2.play();
+});
+const item3 = document.getElementById("item3");
+
+item3.addEventListener("click", function () {
+  const audioClick3 = new Audio("./audio/crow.mp3");
+  audioClick3.play();
+});
+const item4 = document.getElementById("item4");
+
+item4.addEventListener("click", function () {
+  const audioClick4 = new Audio("./audio/dog.mp3");
+  audioClick4.play();
+});
+const item5 = document.getElementById("item5");
+
+item5.addEventListener("click", function () {
+  const audioClick5 = new Audio("./audio/eagle.mp3");
+  audioClick5.play();
+});
+const item6 = document.getElementById("item6");
+
+item6.addEventListener("click", function () {
+  const audioClick6 = new Audio("./audio/elephant.mp3");
+  audioClick6.play();
+});
+const item7 = document.getElementById("item7");
+
+item7.addEventListener("click", function () {
+  const audioClick7 = new Audio("./audio/horse.mp3");
+  audioClick7.play();
+});
+const item8 = document.getElementById("item8");
+
+item8.addEventListener("click", function () {
+  const audioClick8 = new Audio("./audio/sheep.mp3");
+  audioClick8.play();
+});
+const item9 = document.getElementById("item9");
+
+item9.addEventListener("click", function () {
+  const audioClick9 = new Audio("./audio/tiger.mp3");
+  audioClick9.play();
+});
